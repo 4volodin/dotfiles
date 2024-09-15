6644e5856cd6145ec78a5d1892ca9557f79c9b3e
Aleks Volodin <4volodin@gmail.com>
2024-09-15 17:32:05 +0400

    Disable custom lfcd

--- .zsh-functions.zsh
+++ .zsh-functions.zsh
@@ -217,19 +217,23 @@ lfj () {
 	return $?
 }
 lfcd () {
-    tmp="$(mktemp)"
-    lf -last-dir-path="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp"
-        if [ -d "$dir" ]; then
-            if [ "$dir" != "$(pwd)" ]; then
-                cd "$dir"
-            fi
-        fi
-    fi
+    # `command` is needed in case `lfcd` is aliased to `lf`
+    cd "$(command lf -print-last-dir "$@")"
 }
-bindkey -s '^o' '^ulfcd\n'
+# lfcd () {
+#     tmp="$(mktemp)"
+#     lf -last-dir-path="$tmp" "$@"
+#     if [ -f "$tmp" ]; then
+#         dir="$(cat "$tmp")"
+#         rm -f "$tmp"
+#         if [ -d "$dir" ]; then
+#             if [ "$dir" != "$(pwd)" ]; then
+#                 cd "$dir"
+#             fi
+#         fi
+#     fi
+# }
+# bindkey -s '^o' '^ulfcd\n'
 
 ## {{{-------------------- nginx--------------------
 function nginxcreate() {
